c41d82584e5841a2aa87262d2b0839e87ba1bc79
ibo12224 <113340577+ibo12224@users.noreply.github.com>
2022-12-03 11:39:57 +0900

    123

--- visual_exam20193074/Program.cs
+++ visual_exam20193074/Program.cs
@@ -1,18 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace visual_exam20193074
+using System.Threading;
+namespace visualexam6_14_2
 {
-    internal class Program
+    class Account
+    {
+        private double balance;
+        public Account(double initialdeposit) { balance = initialdeposit; }
+        public double Balance{
+            get { return balance; }
+            set { balance = value; }
+        }
+        public void Deposit(double amount)
+        {
+            lock (this)
+            {
+                balance += amount;
+            }
+        }
+    }
+    class Teller
+    {
+        string name;
+        Account account;
+        double amount;
+        public Teller(string name, Account account, double amount)
+        {
+            this.name = name;
+            this.account = account;
+            this.amount = amount;
+        }
+        public void TellerTask()
+        {
+            account.Deposit(amount);
+            Console.WriteLine(name + " : " + account.Balance);
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
-
-
-
+            Account acc = new Account(3);
+            Teller tel = new Teller("nam", acc, 300);
+            tel.TellerTask();
+            acc.Balance = 100;
+            tel.TellerTask();
+            acc.Balance = 10;
+            tel.TellerTask();
+            Teller tl = new Teller("nad", acc, 100);
+            tl.TellerTask();
+            acc.Balance = 10;
+            tl.TellerTask();
 
         }
     }
